67968dfc709be7bd8285ead8e27304e91149808a
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-05-17 20:21:22 -0300

    feat: Initialized tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Fragment } from 'react'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Fragment>
+      <h1 className='bg-red-900'>
+      eae
+      </h1>
+    </Fragment>
+  )
+}
+
+export default Home
